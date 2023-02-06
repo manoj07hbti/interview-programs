d457d66ef81e835c46f37e5f6f6a95e9602cec21
60manojkumar@gmail.com <Motivated@2019>
2023-02-06 15:56:53 +0530

    Java 8 Interview Programs

--- src/java_8_programms/EmployeeMaxMinWithCollectors.java
+++ src/java_8_programms/EmployeeMaxMinWithCollectors.java
@@ -16,17 +16,26 @@ class EmployeeMaxMinWithCollectors {
             new Employee("Deborah Sprightly", 29, 9000.00));
 
     public static void main(String[] args) {
-
+//TODO 1. find employee with max salary
         Optional<Employee> maxSalaryEmp =
                 employeeList.stream()
                         .collect(Collectors.maxBy(Comparator.comparing(Employee::getSalary)));
         System.out.println("Employee with max salary:"
                 + (maxSalaryEmp.isPresent()? maxSalaryEmp.get():"Not Applicable"));
 
+//TODO here we have created stream from list of String using .stream()
+//  and we have used Collectors.maxBy method to get the max salary by passing employee salary inside comparing method
+
+
+//TODO 2. find employee with minimum age
 
         Optional<Employee> minAgeEmp =
                 employeeList.stream()
                         .collect(Collectors.minBy(Comparator.comparing(Employee::getAge)));
+//TODO here we have created stream from list of String using .stream()
+//  and we have used Collectors.minBy method to get the min Age by passing employee salary inside comparing method
+
+
         System.out.println("Employee with min age:"
                 + (minAgeEmp.isPresent()? minAgeEmp.get():"Not Applicable"));
     }
